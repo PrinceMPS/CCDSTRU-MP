88bae50323bcee4fc2ee489bdc2615d7c020abf6
JR Santiago <jrsantiago3001@gmail.com>
2025-03-27 13:49:29 +0800

    Create variable declarations

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include "colors.h"
 
+typedef struct
+{
+    int x;
+    int y;
+} posType; //struct for positions on the board
+
+typedef struct 
+{
+    posType position[16];
+    int     numPositions;
+} playerType; //struct for UNO and TRES
+
 int main()
 {
+    playerType UNO,
+               TRES;
+    int        turn = 1; // True
+    int        go = 0; // False
+
     return 0;
 }
