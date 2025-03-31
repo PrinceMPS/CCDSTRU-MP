2d16dcd1a4d80dad6d292a9ee5287ea8cb25761b
PrinceMPS <prinmat88@gmail.com>
2025-03-31 16:46:51 +0800

    restructure program

--- main.c
+++ main.c
@@ -1,75 +1,92 @@
 #include <stdio.h>
 #include "colors.h"
 
-typedef struct
+void moveUnoTres(char board[][4], int *playerCount, char player)
 {
-    int x;
-    int y;
-} posType; //struct for positions on the board
+    int     x,
+            y,
+            nValid = 0;
+    
+    do {
+        printf("Input coordinate or smth\n");
+        scanf("%d,%d", &x, &y);
+        if (board[x-1][y-1] == '\0')
+        {
+            nValid = 1;
+            (*playerCount)++;
+            board[x-1][y-1] = player;
+            break;
+        }
+        else
+        {
+            printf("Try again bobo\n");
+        }
 
-typedef struct 
+    }while (nValid == 0);
+
+}
+
+
+void dosMove(char board[][4], int *unoCount, int *tresCount)
 {
-    posType position[16];
-    int     numPositions;
-} playerType; //struct for UNO and TRES
+    int x,
+        y,
+        nValid = 0;
+        do 
+        {
+            printf("Input coordinate or smth\n");
+            scanf("%d,%d", &x, &y);
+            if (board[x-1][y-1] == '1' || board[x-1][y-1] == '3')
+            {
+                nValid = 1;
+                if (board[x-1][y-1] == '1')
+                {
+                    (*unoCount)--;
+                }
+                else
+                {
+                    (*tresCount)--;
+                }
+                board[x-1][y-1] = '\0';
+                break;
+            }
+            else
+            {
+                printf("Try again bobo\n");
+            }
+    
+        }while (nValid == 0);
+}
 
-typedef playerType winType[4]; //struct array for win conditions
-
-/*FUNCTION PROTOTYPES*/
-
-void initializeWin(winType W);
-int searchPosition();
-int checkWin();
-void deletePosition();
-
-/*FUNCTION DEFINITIONS*/
-void initializeWin(winType W)
+void checkWin()
 {
-    int i, j;
 
-    // Win condition 1: Top rows
-    for(i = 1; i <= 4; i++)
-    {
-        W[0].numPositions = 4;
-        W[0].position[i-1].x = 1;
-        W[0].position[i-1].y = i;
-    }
-    // Win condition 2: Top left to bottom right
-    for(i = 1; i <= 4; i++)
-    {
-        W[1].numPositions = 4;
-        W[1].position[i-1].x = i;
-        W[1].position[i-1].y = i;
-    }
-    // Win condition 3: Top right to bottom left
-    for(i = 1, j = 4; i <= 4; i++, j--)
-    {
-        W[2].numPositions = 4;
-        W[2].position[i-1].x = i;
-        W[2].position[i-1].y = j;
-    }
-    // Win condition 4: Bottom rows
-    for(i = 1; i <= 4; i++)
-    {
-        W[3].numPositions = 4;
-        W[3].position[i-1].x = 4;
-        W[3].position[i-1].y = i;
-    }
 }
 
 int main()
 {
-    playerType UNO,
-               TRES;
-    posType    pos; // inputted position will be placed here first for checking before being assigned to a player
-    winType    W; // Win conditions
-    int        turn = 1; // True
-    int        go = 0; // False
+    int     i,
+            j,
+            unoCount = 0,
+            tresCount = 0,
+            turn = 1,
+            go = 0;
 
-    UNO.numPositions = 0;
-    TRES.numPositions = 0;
+    char board[4][4];
 
-    initializeWin(W);
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            board[i][j] = '\0';
+        }
+    }
+    while (unoCount + tresCount != 16)
+    {
+        moveUnoTres(board, &tresCount, '3');
+        moveUnoTres(board, &unoCount, '1');
+        dosMove(board, &unoCount, &tresCount);
+    }
 
     return 0;
 }
